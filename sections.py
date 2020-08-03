d1fdc7e2787430f0ff676bd53a865699d4d0439b
Oliver Koch <n52e09@gmail.com>
2020-08-03 12:43:58 +0200

    sections.py: added more section types

--- sections.py
+++ sections.py
@@ -31,12 +31,19 @@ def verts_circle(r):
     return [arc(0, 0, r, 0, 360, n=72)[:-1]], []
 
 def verts_tube(ra, ri):
+    """circular tube"""
     outer = arc(0, 0, ra, 0, 360, n=72)[:-1]
     inner = arc(0, 0, ri, 360, 0, n=72)[:-1]
     return [outer], [inner]
 
 def verts_c1(h, tw, ba, ta, bf, tf):
-    """c profile, no fillets"""
+    """c profile, no fillets. 
+    
+    Corresponds to:
+    - Metal_Stg_C_01_b
+    - Metal_Fra_C_01_c
+    - Metal_Stg_L_02_b (bf -> blw, tf -> blw)
+    """
     vertices = np.array([
         (ba, 0),
         (ba, ta),
@@ -50,7 +57,13 @@ def verts_c1(h, tw, ba, ta, bf, tf):
     return [vertices], []
 
 def verts_c2(h, tw, ba, ra, bf, rf):
-    """c profile, sheet metal"""
+    """c profile, sheet metal
+    
+    Corrensponds to: 
+    - Metal_Stg_C_01_c
+    - Metal_Stg_L_02_c (bf -> blw)
+    - Metal_Fra_C_05_b
+    """
     arrays = [
         [(ba, 0), (ba, tw)],
         arc(ra+tw, ra+tw, ra, 270, 180),
@@ -61,9 +74,53 @@ def verts_c2(h, tw, ba, ra, bf, rf):
     ]
     return [np.concatenate(arrays)], []
         
+def verts_c3(h, tw, ba, ta, ra, bf, tf, rf):
+    """c profile, with fillets, machined or extruded
+    
+    Corrensponds to: 
+    - Metal_Stg_C_01_a
+    - Metal_Fra_C_01_c
+    - Metal_Stg_L_02_a (blw -> bf, tlf -> tf, rlw -> rf)
+    """
+    arrays = [
+        [(ba, 0), 
+        (ba, tw)],
+        arc(ra+tw, ra+ta, ra, 270, 180),
+        arc(rf+tw, h-tf-rf, rf, 180, 90),
+        [(bf, h-tf), (bf, h), (0, h), (0, 0)]
+    ]
+    return [np.concatenate(arrays)], []
+        
+
+def verts_e1(h, hs, twa, twf, ba, ta, bs, ts, bf, tf):
+    """e profile, no fillets.
+    
+    Corresponds to:
+    - Metal_Fra_E_01_d
+    """
+    vertices = np.array([
+        (ba, 0),
+        (ba, ta),
+        (twa, ta),
+        (twa, hs),
+        (ts, hs),
+        (ts, hs+bs),
+        (twf, hs+bs),
+        (twf, h-tf),
+        (bf, h-tf),
+        (bf, h),
+        (0, h),
+        (0, 0)
+    ])
+    return [vertices], []
+
         
 def verts_z1(h, tw, ba, ta, bf, tf):
-    """z profile, no fillets"""
+    """z profile, no fillets.
+    
+    Corresponds to:
+    - Metal_Stg_Z_01_b
+    """
     vertices = np.array([
         (tw, 0),
         (tw, h-tf),
@@ -78,7 +135,11 @@ def verts_z1(h, tw, ba, ta, bf, tf):
 
 
 def verts_z2(h, tw, ba, ra, bf, rf):
-    """Z section, sheet metal"""
+    """Z section, sheet metal
+    
+    Corresponds to:
+    - Metal_Stg_Z_01_c
+    """
     arrays = [
         arc(-ra, ra+tw, ra+tw, 270, 360),
         arc(rf+tw, h-tw-rf, rf, 180, 90),
@@ -90,8 +151,13 @@ def verts_z2(h, tw, ba, ra, bf, rf):
     return [np.concatenate(arrays)], []
 
 
-def verts_z02c(h, tw, ba, ra, bf, rf, blf, rlf):
-    """Z section, with lipped flange, sheet metal. ISAMI Fra-Z-02-b or Stg-Z-02-c"""
+def verts_z3(h, tw, ba, ra, bf, rf, blf, rlf):
+    """Z section, with one lipped flange, sheet metal.
+
+    Corresponds to:
+    - Metal_Fra_Z_02_b
+    - Metal_Stg_Z_02_c
+    """
     arrays = [
         arc(-ra, ra+tw, ra+tw, 270, 360),
         arc(rf+tw, h-tw-rf, rf, 180, 90),
@@ -104,8 +170,78 @@ def verts_z02c(h, tw, ba, ra, bf, rf, blf, rlf):
     ]
     return [np.concatenate(arrays)], []
 
+
+def verts_z4(h, tw, ba, ta, bf, tf, blf, tlf):
+    """Z section, with one lipped flange, no fillets.
+
+    Corresponds to:
+    - Metal_Stg_Z_02_b
+    """
+    vertices = np.array([
+        (tw, 0),
+        (tw, h-tf),
+        (bf-tlf, h-tf),
+        (bf-tlf, h-blf),
+        (bf, h-blf),
+        (bf, h),
+        (0, h),
+        (0, ta),
+        (tw-ba, ta),
+        (tw-ba, 0)
+    ])
+    return [vertices], []
+
+
+def verts_z5(h, tw, ba, ta, ra, bf, tf, rf):
+    """Z section, with fillets.
+    
+    Corresponds to:
+    - Metal_Stg_Z_01_a
+    """
+    arrays = [
+        arc(rf+tw, h-tf-rf, rf, 180, 90),
+        [(bf, h-tf), (bf, h), (0, h)],
+        arc(-ra, ta+ra, 360, 270),
+        [(tw-ba, ta), (tw-ba, 0)]
+    ]
+    return [np.concatenate(arrays)], []
+
+
+def verts_z6(h, tw, ba, ta, rf):
+    """Z section, round flange, no fillets.
+    
+    Corresponds to:
+    - Metal_Stg_Z_04_b
+    """
+    arrays = [
+        arc(rf, h-rf, rf-tw, 180, 0),
+        arc(rf, h-rf, rf, 0, 180),
+        [(0, ta), (tw-ba, ta), (tw-ba, 0), (0, 0)]
+    ]
+    return [np.concatenate(arrays)], []
+
+def verts_z8(h, tw, ba, ta, ra, rf):
+    """Z section, round flange, fillets.
+    
+    Corresponds to:
+    - Metal_Stg_Z_04_a
+    """
+    arrays = [
+        arc(rf, h-rf, rf-tw, 180, 0),
+        arc(rf, h-rf, rf, 0, 180),
+        arc(-ra, ta+ra, ra, 360, 270),
+        [(tw-ba, 0), (0, 0)]
+    ]
+    return [np.concatenate(arrays)], []
+
+
 def verts_i1(h, tw, ba, ta, bf, tf):
-    """symmetric I section, no fillets"""
+    """symmetric I section, no fillets
+    
+    Corrensponds to: 
+    - Metal_Stg_I_01_b
+    - Metal_Fra_I_01_d
+    """
     vertices = np.array([
         (ba/2, 0),
         (ba/2, ta),
@@ -124,7 +260,11 @@ def verts_i1(h, tw, ba, ta, bf, tf):
 
 
 def verts_j1(h, tw, ba, ta, bf, tf):
-    """J section, no fillets"""
+    """J section, no fillets.
+    
+    Corresponds to:
+    - Metal_Stg_J_01_b
+    """
     vertices = np.array([
         ((ba-tw)/2, 0),
         ((ba-tw)/2, ta),
@@ -140,8 +280,63 @@ def verts_j1(h, tw, ba, ta, bf, tf):
     return [vertices], []
 
 
+def verts_j2(h, tw, ba, ta, ra, bf, tf, rf):
+    """J section, with fillets.
+    
+    Corresponds to:
+    - Metal_Stg_J_01_a
+    """
+    arrays = [
+        [((ba-tw)/2, 0), ((ba-tw)/2, ta)],
+        arc(ra+tw, ra+ta, ra, 270, 180),
+        arc(rf+tw, h-tf-rf, rf, 180, 90),
+        [(bf, h-tf), (bf, h), (0, h)],
+        arc(-ra, ta+ra, 360, 270),
+        [((tw-ba)/2, ta), ((tw-ba)/2, 0)],
+    ]
+    return [np.concatenate(arrays)], []
+
+
+def verts_t1(h, tw, ba, ta):
+    """T section, no fillets.
+    
+    Corresponds to:
+    - Metal_Stg_T_01_b
+    """
+    vertices = np.array([
+        ((ba-tw)/2, 0),
+        ((ba-tw)/2, ta),
+        (tw, ta),
+        (tw, h),
+        (0, h),
+        (0, ta),
+        ((tw-ba)/2, ta),
+        ((tw-ba)/2, 0),
+    ])
+    return [vertices], []
+
+def verts_t2(h, tw, ba, ta, ra):
+    """T section, with fillets.
+    
+    Corresponds to:
+    - Metal_Stg_T_01_a
+    """
+    arrays = [
+        [((ba-tw)/2, 0), ((ba-tw)/2, ta)],
+        arc(ra+tw, ra+ta, ra, 270, 180),
+        [(tw, h), (0, h)],
+        arc(-ra, ta+ra, 360, 270),
+        [((tw-ba)/2, ta), ((tw-ba)/2, 0)],
+    ]
+    return [np.concatenate(arrays)], []
+
+
 def verts_l1(h, tw, ba, ta):
-    """L section, no fillets"""
+    """L section, no fillets
+    
+    Corrensponds to: 
+    - Metal_Stg_L_01_b
+    """
     vertices = np.array([
         (ba, 0),
         (ba, ta),
@@ -154,7 +349,11 @@ def verts_l1(h, tw, ba, ta):
 
 
 def verts_l2(h, tw, ba, ra):
-    """L section, sheet metal"""
+    """L section, sheet metal
+
+    Corresponds to:
+    - Metal_Stg_L_01_c
+    """
     arrays = [
         [(ba, 0), (ba, tw)],
         arc(ra+tw, ra+tw, ra, 270, 180),
@@ -164,6 +363,20 @@ def verts_l2(h, tw, ba, ra):
     return [np.concatenate(arrays)], []
 
 
+def verts_l3(h, tw, ba, ta, ra):
+    """L section, with fillets
+
+    Corresponds to:
+    - Metal_Stg_L_01_a
+    """
+    arrays = [
+        [(ba, 0), (ba, ta)],
+        arc(ra+tw, ra+ta, ra, 270, 180),
+        [(tw, h), (0, h), (0, 0)]
+    ]
+    return [np.concatenate(arrays)], []    
+
+
 VERTEX_FUNCTIONS = {
     "rect":   (verts_rect, ["h", "b"]),
     "tri":    (verts_tri, ["h", "b"]),
@@ -171,21 +384,30 @@ VERTEX_FUNCTIONS = {
     "tube":   (verts_tube, ["ra", "ri"]),
     "c1":     (verts_c1, ["h", "tw", "ba", "ta", "bf", "tf"]),
     "c2":     (verts_c2, ["h", "tw", "ba", "ra", "bf", "rf"]),
+    "c3":     (verts_c3, ["h", "tw", "ba", "ta", "ra", "bf", "tf", "rf"]),
+    "e1":     (verts_e1, ["h", "hs", "twa", "twf", "ba", "ta", "bs", "ts", "bf", "tf"]),
     "i1":     (verts_i1, ["h", "tw", "ba", "ta", "bf", "tf"]),
     "j1":     (verts_j1, ["h", "tw", "ba", "ta", "bf", "tf"]),
+    "j2":     (verts_j2, ["h", "tw", "ba", "ta", "ra", "bf", "tf", "rf"]),
     "l1":     (verts_l1, ["h", "tw", "ba", "ta"]),
     "l2":     (verts_l2, ["h", "tw", "ba", "ra"]),
+    "l3":     (verts_l3, ["h", "tw", "ba", "ta", "ra"]),
+    "t1":     (verts_t1, ["h", "tw", "ba", "ta"]),
+    "t2":     (verts_t2, ["h", "tw", "ba", "ta", "ra"]),
     "z1":     (verts_z1, ["h", "tw", "ba", "ta", "bf", "tf"]),
     "z2":     (verts_z2, ["h", "tw", "ba", "ra", "bf", "rf"]),
-    "z02c":   (verts_z02c, ["h", "tw", "ba", "ra", "bf", "rf", "blf", "rlf"]),
+    "z3":     (verts_z3, ["h", "tw", "ba", "ra", "bf", "rf", "blf", "rlf"]),
+    "z4":     (verts_z4, ["h", "tw", "ba", "ta", "bf", "tf", "blf", "tlf"]),
+    "z5":     (verts_z5, ["h", "tw", "ba", "ta", "ra", "bf", "tf", "rf"]),
+    "z6":     (verts_z6, ["h", "tw", "ba", "ta", "rf"]),
+    "z8":     (verts_z8, ["h", "tw", "ba", "ta", "ra", "rf"]),
 }
 
 
 def section(kind, *args):
     "Return a shapely polygon"
-    # outer, inner: lists of vertices
     func, attrib_names = VERTEX_FUNCTIONS[kind]
-    #x = [params[k] for k in attrib_names]
+    # outer, inner: lists of vertices
     outer, inner = func(*args)
     #try:
     p = Polygon(outer[0])
@@ -195,6 +417,3 @@ def section(kind, *args):
         p = p.difference(Polygon(verts))
     #assert p.is_valid
     return p if p.is_valid else None
-    #except:
-    #    return None
-    
